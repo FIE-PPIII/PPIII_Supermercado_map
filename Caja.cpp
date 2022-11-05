3404f773a886dabebf6bc34f23a407bcc998d408
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-05 00:22:01 -0300

    ADD - Caja

---
+++ Caja.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Usuario on 11/5/2022.
+//
+
+#include "Caja.h"
+
+using namespace std;
+
+Caja::Caja(const int num) {
+    this->numero = num;
+    this->monto = 0.0;
+}
+
+Caja::~Caja() {}
---
+++ Caja.h
@@ -0,0 +1,37 @@
+//
+// Created by Usuario on 11/5/2022.
+//
+
+#ifndef BASE_CPP_CAJA_H
+#define BASE_CPP_CAJA_H
+
+#include <map>
+
+using namespace std;
+
+class Caja {
+private:
+    int numero;
+    map<int, Venta> ventas;
+    double monto;
+
+public:
+    Caja(const int);
+    ~Caja();
+
+    int obtenerNroCaja() const;
+    const map<int, Venta> & obtenerVentas() const;
+    double obtenerMontoCaja() const;
+};
+
+inline int Caja::obtenerNroCaja() const {
+    return this->numero;
+}
+
+inline const map<int, Venta> & Caja::obtenerVentas() const {
+    return this->ventas;
+}
+
+
+
+#endif //BASE_CPP_CAJA_H
