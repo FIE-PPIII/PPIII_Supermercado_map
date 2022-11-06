d04853ff04d716fc6216fb7507b6aec76a58660e
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-05 23:55:22 -0300

    ADD - Supermercado.ingresarVentas()

--- Supermercado.cpp
+++ Supermercado.cpp
@@ -20,3 +20,34 @@ void Supermercado::ingresarArticulos() {
     this->stock[4545] = Articulo(4545, "Manteca", 87.23, 45);
     this->stock[1212] = Articulo(1212, "Jugo", 52.28, 92);
 }
+
+void Supermercado::ingresarVentas() {
+    Venta venta_1 = Venta();
+    venta_1.insertarArticuloVenta(&(this->stock.at(2323)), 3);
+    venta_1.insertarArticuloVenta(&(this->stock.at(4545)), 2);
+    this->cajas[0].insertarVenta(venta_1);
+
+    Venta venta_2 = Venta();
+    venta_2.insertarArticuloVenta(&(this->stock.at(2323)), 2);
+    this->cajas[0].insertarVenta(venta_2);
+
+    Venta venta_3 = Venta();
+    venta_3.insertarArticuloVenta(&(this->stock.at(2323)), 4);
+    this->cajas[2].insertarVenta(venta_3);
+
+    Venta venta_4 = Venta();
+    venta_4.insertarArticuloVenta(&(this->stock.at(4545)), 2);
+    this->cajas[1].insertarVenta(venta_4);
+
+    Venta venta_5 = Venta();
+    venta_5.insertarArticuloVenta(&(this->stock.at(4545)), 1);
+    this->cajas[1].insertarVenta(venta_5);
+
+    Venta venta_6 = Venta();
+    venta_6.insertarArticuloVenta(&(this->stock.at(4545)), 2);
+    this->cajas[3].insertarVenta(venta_6);
+
+    Venta venta_7 = Venta();
+    venta_7.insertarArticuloVenta(&(this->stock.at(1212)), 2);
+    this->cajas[0].insertarVenta(venta_7);
+}
--- Supermercado.h
+++ Supermercado.h
@@ -34,6 +34,7 @@ public:
     string obtenerDireccion() const;
 
     void ingresarArticulos();
+    void ingresarVentas();
 
 };
 
