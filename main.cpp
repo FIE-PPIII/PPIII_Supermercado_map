dc50f50d9d19920858e887cb40771e322aff359a
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-06 00:39:12 -0300

    MOD - main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "Supermercado.h"
 
 using namespace std;
 
 int main()
 {
-    cout<<"Hello, world!"<<endl;
+    Supermercado super = Supermercado("Manolo", "Callao 653");
+    super.ingresarArticulos();
+    super.mostrarArticulos("\nDatos de los articulos");
+    super.ingresarVentas();
+    super.mostrarMontoCajas("\nMonto Caja");
+    super.mostrarArticulos("\nDatos de los articulos");
+    super.mostrarVentasCaja("\nDetalle de ventas por caja");
+
 
     return 0;
 }
