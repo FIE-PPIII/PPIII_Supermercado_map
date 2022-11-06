85a46d7f112016bdebc3383e802ccba7981f09f2
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-11-05 22:12:02 -0300

    MOD - Articulo

--- Articulo.h
+++ Articulo.h
@@ -23,10 +23,12 @@ public:
     void asignarCodigo(const int);
     void asignarNombre(const string);
     void asignarPrecio(const double);
+    void asignarCantidad(const int);
 
     int obtenerCodigo() const;
     string obtenerNombre() const;
     double obtenerPrecio() const;
+    int obtenerCantidad() const;
 };
 
 inline void Articulo::asignarCodigo(const int cod) {
@@ -41,6 +43,10 @@ inline void Articulo::asignarPrecio(const double p) {
     this->precio = p;
 }
 
+inline void Articulo::asignarCantidad(const int c) {
+    this->cantidad = c;
+}
+
 inline int Articulo::obtenerCodigo() const {
     return this->codigo;
 }
@@ -53,6 +59,9 @@ inline double Articulo::obtenerPrecio() const {
     return this->precio;
 }
 
+inline int Articulo::obtenerCantidad() const {
+    return this->cantidad;
+}
 
 
 #endif //BASE_CPP_ARTICULO_H
